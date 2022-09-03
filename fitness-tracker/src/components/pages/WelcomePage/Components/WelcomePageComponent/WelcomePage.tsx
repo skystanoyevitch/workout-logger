8880189bd67c40007ebf532220d969858ffa07aa
Sky <RolyPolie@users.noreply.github.com>
2022-09-03 15:40:47 -0700

    fixed file paths + removed logic code

--- fitness-tracker/src/components/pages/WelcomePage/Components/WelcomePageComponent/WelcomePage.tsx
+++ fitness-tracker/src/components/pages/WelcomePage/Components/WelcomePageComponent/WelcomePage.tsx
@@ -1,22 +1,8 @@
 import React, { useState, useRef } from "react";
-// import { PropsFace } from "../../../../../App";
 import styles from "./welcomePage.module.css";
+import InputName from "../logicComponents/inputName";
 
 function WelcomePage(): JSX.Element {
-	const [name, setName] = useState("");
-
-	let nameRef = useRef(null);
-
-	const onClickHandler = (e: any) => {
-		e.preventDefault();
-
-		let nameInput = nameRef.current.value;
-		console.log(nameInput);
-
-		setName(nameInput);
-		nameRef.current.value = "";
-	};
-
 	return (
 		<div className={styles.container}>
 			<h1 className={styles.heading}>
@@ -26,26 +12,7 @@ function WelcomePage(): JSX.Element {
 			<h3 className={styles.subheading}>
 				to start, please type in your name
 			</h3>
-			{/* <label htmlFor="name">Enter Name</label> */}
-
-			<div className={styles.input_container}>
-				<input
-					type="text"
-					name="name"
-					placeholder="name"
-					className={styles.input}
-					ref={nameRef}
-				/>
-				<button
-					type="submit"
-					className={styles.input_button}
-					onClick={onClickHandler}
-				>
-					Enter
-				</button>
-			</div>
-
-			<h2>{name}</h2>
+			<InputName />
 		</div>
 	);
 }
