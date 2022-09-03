5d099b585d79f964fb2726adf2f4672f9acf6e20
Sky <RolyPolie@users.noreply.github.com>
2022-09-03 15:39:58 -0700

    removed comments

--- fitness-tracker/src/App.tsx
+++ fitness-tracker/src/App.tsx
@@ -1,15 +1,7 @@
 import React from "react";
 import { WelcomePage } from "../src/components/pages";
 
-// export interface PropsFace {
-// 	msg: String;
-// }
-
 function App() {
-	// const messageProps: PropsFace = {
-	// 	msg: "WELCOME TO YOUR TRAINING LOG",
-	// };
-
 	return (
 		<div className="">
 			<WelcomePage />
