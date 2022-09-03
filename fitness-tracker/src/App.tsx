0a1d9cb8b8f106554694d59c0f899851202e9d97
Sky <RolyPolie@users.noreply.github.com>
2022-09-02 21:40:33 -0700

    added styles + removed unused code

--- fitness-tracker/src/App.tsx
+++ fitness-tracker/src/App.tsx
@@ -1,21 +1,18 @@
-// import WelcomePage from "./components/pages/WelcomePage/Components/WelcomePageComponent/WelcomePage";
 import React from "react";
 import { WelcomePage } from "../src/components/pages";
 
-export interface PropsFace {
-	msg: String;
-}
+// export interface PropsFace {
+// 	msg: String;
+// }
 
 function App() {
-	const messageProps: PropsFace = {
-		msg: "WELCOME TO YOUR TRAINING LOG",
-	};
+	// const messageProps: PropsFace = {
+	// 	msg: "WELCOME TO YOUR TRAINING LOG",
+	// };
 
 	return (
 		<div className="">
-			<p>
-				This is the <WelcomePage message={messageProps} />{" "}
-			</p>
+			<WelcomePage />
 		</div>
 	);
 }
--- fitness-tracker/src/components/pages/WelcomePage/Components/WelcomePageComponent/WelcomePage.tsx
+++ fitness-tracker/src/components/pages/WelcomePage/Components/WelcomePageComponent/WelcomePage.tsx
@@ -1,13 +1,36 @@
 import React from "react";
-import { PropsFace } from "../../../../../App";
+// import { PropsFace } from "../../../../../App";
 import styles from "./welcomePage.module.css";
 
-interface Iprops {
-	message: PropsFace;
-}
+// interface Iprops {
+// 	message: PropsFace;
+// }
 
-function WelcomePage({ message }: Iprops): JSX.Element {
-	return <div className={styles.text}>{message.msg}</div>;
+function WelcomePage(): JSX.Element {
+	return (
+		<div className={styles.container}>
+			<h1 className={styles.heading}>
+				WELCOME TO YOUR{" "}
+				<span className={styles.text_sub_color}>TRAINING LOG</span>
+			</h1>
+			<h3 className={styles.subheading}>
+				to start, please type in your name
+			</h3>
+			{/* <label htmlFor="name">Enter Name</label> */}
+
+			<div className={styles.input_container}>
+				<input
+					type="text"
+					name="name"
+					placeholder="name"
+					className={styles.input}
+				/>
+				<button type="submit" className={styles.input_button}>
+					Enter
+				</button>
+			</div>
+		</div>
+	);
 }
 
 export default WelcomePage;
