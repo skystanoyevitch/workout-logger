5f47eb0fbeeead58776416d606dbd9ff21a64a9a
Sky <RolyPolie@users.noreply.github.com>
2022-09-08 20:40:04 -0700

    added react router imports + removed old code

--- fitness-tracker/src/App.tsx
+++ fitness-tracker/src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { WelcomePage } from "../src/components/pages";
+import { FrontPage } from "./components/pages/FrontPage/Components/FrontPage";
 
 function App() {
 	return (
--- fitness-tracker/src/main.tsx
+++ fitness-tracker/src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { FrontPage } from "./components/pages/FrontPage/Components/FrontPage";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+	<BrowserRouter>
+		<Routes>
+			<Route path="/" element={<App />} />
+			<Route path="/frontpage" element={<FrontPage />} />
+			<Route />
+		</Routes>
+	</BrowserRouter>
+);
