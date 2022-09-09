ddc08e3d365b07a26373faf9b1f057033b31fd12
Sky <RolyPolie@users.noreply.github.com>
2022-09-08 20:41:30 -0700

    added useLocation from react router

--- fitness-tracker/src/components/pages/FrontPage/Components/FrontPage.tsx
+++ fitness-tracker/src/components/pages/FrontPage/Components/FrontPage.tsx
@@ -1,11 +1,24 @@
 import React from "react";
 import Styles from "../Styles/FrontPage.module.css";
+import { useLocation, Location } from "react-router-dom";
+
+declare function useLocation(): Location;
+
+interface Location {
+	state: any;
+}
 
 export function FrontPage(): JSX.Element {
+	const location: Location = useLocation();
+	const username = location?.state.username.toUpperCase();
+
+
 	return (
 		<div className={Styles.container}>
 			<h1 className={Styles.heading}>
-				<span className={Styles.text_sub_color}>SKYS TRAINING LOG</span>
+				<span className={Styles.text_sub_color}>
+					{username}'S TRAINING LOG
+				</span>
 			</h1>
 			<h3 className={Styles.subheading}>
 				to start, please type in your name
