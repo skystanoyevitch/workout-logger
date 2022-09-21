a2a37e0d1ccc25a4f936b53406f574a170748686
Sky <RolyPolie@users.noreply.github.com>
2022-09-20 22:26:13 -0700

    added workout logic + form

--- workout-logger/src/pages/TrainingLogPage/TrainingLog.tsx
+++ workout-logger/src/pages/TrainingLogPage/TrainingLog.tsx
@@ -1,20 +1,49 @@
-import { Container, Typography, Button } from "@mui/material";
+import { Container, Typography } from "@mui/material";
 import { InputForm } from "./Components/InputForm";
-import { Link } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+import { useState } from "react";
 
+// type nameInput = {
+// 	username: string;
+// };
+
+type UsernameType = {
+	username: string;
+}
+
+// type UpperCaseName = Uppercase<UsernameType>;
 const TrainingLog = (): JSX.Element => {
-	const username: string | undefined = localStorage
-		.getItem("username")
-		?.toUpperCase();
+	const [workout, setWorkout] = useState({
+		name: "",
+		date: new Date().toLocaleDateString("en-US"),
+		time: new Date().toLocaleTimeString("en-US"),
+		notes: "",
+	});
+
+	const location = useLocation();
+	const username = location.state as UsernameType;
+
+	const onSubmitHandler = (e: React.FormEvent) => {};
 
 	return (
-		<Container maxWidth="md" sx={{ textAlign: "center", marginTop: 10 }}>
-			<Typography variant="h1">{username}'S TRAINING LOG</Typography>
-			<Typography variant="body1" paddingY={4}>
-				lets add some details to begin...
-			</Typography>
-			<InputForm />
-		</Container>
+		<>
+			<Container
+				maxWidth="md"
+				sx={{ textAlign: "center", marginTop: 10 }}
+			>
+				<Typography component="h1" variant="h1">
+					WELCOME {username.toString().toUpperCase()}
+				</Typography>
+				<Typography variant="body1" paddingY={4}>
+					lets add some details to begin...
+				</Typography>
+				<InputForm
+					workout={workout}
+					setWorkout={setWorkout}
+					onSubmitHandler={onSubmitHandler}
+				/>
+			</Container>
+		</>
 	);
 };
 
