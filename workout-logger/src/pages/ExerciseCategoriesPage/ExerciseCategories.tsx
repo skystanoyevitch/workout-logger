2f17a82f57f30f4140c7c4e0157ccef7d2f18830
Sky <RolyPolie@users.noreply.github.com>
2022-09-21 15:49:25 -0700

    added ability to dynamically render content

--- workout-logger/src/pages/ExerciseCategoriesPage/ExerciseCategories.tsx
+++ workout-logger/src/pages/ExerciseCategoriesPage/ExerciseCategories.tsx
@@ -1,75 +1,65 @@
-import { Grid, Typography } from "@mui/material";
+import { Button, Grid, Typography } from "@mui/material";
 import { Container } from "@mui/system";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
-export const ExerciseCategories = (): JSX.Element => {
+import db from "../../db/db.json";
+import { useState } from "react";
+import { ExerciseList } from "../ExerciseListPage/ExerciseList";
+
+export const ExerciseCategories = ({ onHandleNameClick }): JSX.Element => {
+	const [openComponent, setOpenComponent] = useState(false);
+	const [nameOfExercise, setNameOfExercise] = useState("");
+	// const location = useLocation();
+	// const getExerciseName = location.state;
+	// console.log(db.category[0].exercises);
+
+	const onClickHandler = (exerciseName) => {
+		setOpenComponent(true);
+		setNameOfExercise(exerciseName);
+	};
 	return (
 		<>
 			<Container sx={{ textAlign: "center", marginTop: 10 }}>
-				<Typography variant="h1">Select Exercise Category</Typography>
-				<Grid
-					container
-					justifyContent="center"
-					gap={4}
-					sx={{ paddingTop: 8 }}
-				>
-					<Grid
-						item
-						sm={4}
-						sx={{ padding: 4, border: "2px solid black" }}
-					>
-						<Link to="/exerciselist">CHEST</Link>
-					</Grid>
-					<Grid
-						item
-						sm={4}
-						sx={{ padding: 4, border: "2px solid black" }}
-					>
-						BACK
-					</Grid>
-					<Grid
-						item
-						sm={4}
-						sx={{ padding: 4, border: "2px solid black" }}
-					>
-						SHOULDERS
-					</Grid>
-					<Grid
-						item
-						sm={4}
-						sx={{ padding: 4, border: "2px solid black" }}
-					>
-						ABS
-					</Grid>
-					<Grid
-						item
-						sm={4}
-						sx={{ padding: 4, border: "2px solid black" }}
-					>
-						LEGS
-					</Grid>
-					<Grid
-						item
-						sm={4}
-						sx={{ padding: 4, border: "2px solid black" }}
-					>
-						BICEPS
-					</Grid>
-					<Grid
-						item
-						sm={4}
-						sx={{ padding: 4, border: "2px solid black" }}
-					>
-						TRICEPS
-					</Grid>
-					<Grid
-						item
-						sm={4}
-						sx={{ padding: 4, border: "2px solid black" }}
-					>
-						CARDIO
-					</Grid>
-				</Grid>
+				{openComponent ? (
+					<ExerciseList
+						nameOfExercise={nameOfExercise}
+						onHandleNameClick={onHandleNameClick}
+					/>
+				) : (
+					<>
+						<Typography variant="h1">
+							Select Exercise Category
+						</Typography>
+						<Grid
+							container
+							justifyContent="center"
+							gap={4}
+							sx={{ paddingTop: 8 }}
+						>
+							{db.category.map((name, index) => (
+								// console.log(name.categoryName)
+
+								<Grid
+									item
+									sm={4}
+									sx={{
+										padding: 4,
+										border: "2px solid black",
+									}}
+									key={index}
+								>
+									<Button
+										onClick={() =>
+											onClickHandler(name.exercises)
+										}
+									>
+										{name.categoryName}
+									</Button>
+								</Grid>
+							))}
+						</Grid>
+					</>
+				)}
 			</Container>
 		</>
 	);
